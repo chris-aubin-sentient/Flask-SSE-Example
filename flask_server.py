fd174c3ca7a10c97c8292382db362c4313c10303
chris-aubin-sentient <christopher@sentient.xyz>
2025-03-13 14:14:13 -0700

    Clean up code

--- flask_server.py
+++ flask_server.py
@@ -1,12 +1,16 @@
-from flask import Flask, Response
 import time
+from flask import Flask, Response
+
+
 app = Flask(__name__)
+
 
 def generate_data():
     for i in range(10):
         # The "data:" prefix and the "\n\n" suffix are required
         yield f"data: Data chunk {i}\n\n"
         time.sleep(1)
+
 
 @app.route('/stream')
 def stream():
@@ -20,6 +24,7 @@ def stream():
             'X-Accel-Buffering': 'no' 
         }
     )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
